c55e558ae77a8633361faed76aff537ac48d705e
lisa <lisa_qiu@qq.com>
2019-01-24 18:27:33 +0800

    第二章代码增加注释

--- 02_first_session.py
+++ 02_first_session.py
@@ -1,7 +1,9 @@
 import tensorflow as tf 
 
+#tf.constant代表常量
 a = tf.constant([1.0,2.0],name='a')
 b = tf.constant([2.0,3.0],name='b')
 result = a + b
+#Session会话用于计算结果
 sess = tf.Session()
 print(sess.run(result))
