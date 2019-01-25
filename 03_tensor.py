e11f87749ba5364c6ddc924a414332520ad95b08
lisa <lisa_qiu@qq.com>
2019-01-25 15:26:35 +0800

    03张量

--- 03_tensor.py
+++ 03_tensor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 import tensorflow as tf 
-# tf.constant 是一个计算，这个计算的结果为一个张量，保存在变量a中
+# tf.constant 是一个计算，这个计算的结果为一个张量，保存在变量a中，使用张量可以保存中间结果
 a = tf.constant([1.0,2.0],name='a')
 b = tf.constant([2.0,3.0],name='b')
 result = tf.add(a,b,name='add')
