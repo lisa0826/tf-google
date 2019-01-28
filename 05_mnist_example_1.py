761be1c5cd088622e541671dee13d31513e2a060
lisa <lisa_qiu@qq.com>
2019-01-28 18:24:54 +0800

    05MNIST数据集训练

---
+++ 05_mnist_example_1.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 载入MNIST数据集，如果指定地址/path/to/MNIST_data下没有已经下载好的数据，那么tensorflow会自动从表5-1给出的网址下载数据
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+# 打印Training data size:55000
+print "Training data size:",mnist.train.num_examples
+
+# 打印Validating data size:5000
+print "Validating data size:",mnist.validation.num_examples
+
+# 打印Testing data size:10000
+print "Testing data size:",mnist.test.num_examples
+
+# 打印Example training data:
+print "Example training data:",mnist.train.images[0]
+
+# 打印Example training data label:
+print "Example training data label:",mnist.train.labels[0]
